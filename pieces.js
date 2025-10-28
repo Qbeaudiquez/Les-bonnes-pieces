14e50ad4fe9df440eafb96df5b49cd4f85b31bc5
Quentin Beaudiquez <q.beaudiquez.pro@gmail.com>
2025-10-28 13:40:48 +0100

    mise en place de la boucle for pour afficher tous les articles

--- pieces.js
+++ pieces.js
@@ -2,25 +2,36 @@
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-// Création des balises
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pasde description pour le moemnt.";
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-//Rattachement de nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibiliteElement);
+
+
+for (let i = 0; i < pieces.length; i++){
+        const sectionFiches = document.querySelector(".fiches");
+        const pieceElement = document.createElement("article")
+
+    // Création des balises
+        const imageElement = document.createElement("img");
+        imageElement.src = pieces[i].image;
+        const nomElement = document.createElement("h2");
+        nomElement.innerText = pieces[i].nom;
+        const prixElement = document.createElement("p");
+        prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
+        const categorieElement = document.createElement("p");
+        categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = pieces[i].description ?? "Pasde description pour le moemnt.";
+        const disponibiliteElement = document.createElement("p");
+        disponibiliteElement.innerText = pieces[i].disponibilite ? "En stock" : "Rupture de stock";
+
+
+    //Rattachement de nos balises au DOM
+        sectionFiches.appendChild(pieceElement)
+        pieceElement.appendChild(imageElement)
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(disponibiliteElement);
+
+}
+
